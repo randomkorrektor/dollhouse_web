9b861c946a3c8bf5d4ba11f13711e3359c171d6e
ValeryCh <valerakrasus@gmail.com>
2017-03-23 21:00:24 +0300

    another Blog.js change

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -5,13 +5,13 @@ import {
 } from 'react-router';
 
 export class BlogData extends React.Component {
-   
+
     render() {
         return (
-        <div>
-            <h1> {this.props.header} </h1>;
+            <div>
+                <h1> {this.props.header} </h1>;
             <h2> {this.props.date} </h2>;
-            <img src={this.props.imgSrc} className="img-responsive"/>;
+            <img src={this.props.imgSrc} className="img-responsive" />;
             <h3> {this.props.text} </h3>;
             <h4> <Link to={this.props.fullPostLink}>Read full article...</Link> </h4>;
         </div>
@@ -22,32 +22,41 @@ export default class BlogPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            /*
-                header, 
-                date, 
-                img, 
-                text,
-                fullPostLink
-                */
-            }
+            posts: [{
+                header: "Header title",
+                date: new Date().toLocaleTimeString(),
+                imgSrc: "http://static8.depositphotos.com/1000419/948/v/450/depositphotos_9489492-stock-illustration-valentine-tree-with-hearts-for.jpg",
+                text: "this is small part of artilcle with little sense for",
+                fullPostLink: "/profile"
+            }]
+        }
     }
 
     render() {
+        //const posts = this.state.posts.map((post, i) => <span key={i}>{post.text}</span>)
+        const posts = this.state.posts.map((post, i) =>
+            <BlogData
+                key={i}
+                header={post.header}
+                date={post.date}
+                imgSrc={post.imgSrc}
+                text={post.text}
+                fullPostLink={post.fullPostLink}
+            />
+        )
+
         return (
-             <div>
+            <div>
 
-                 <div>
-                     Blog Page
+                <div>
+                    Blog Page
                      <Header />
-                 </div>  
-                <BlogData 
-                    header="Header title" 
-                    date={new Date().toLocaleTimeString()} 
-                    imgSrc = "http://static8.depositphotos.com/1000419/948/v/450/depositphotos_9489492-stock-illustration-valentine-tree-with-hearts-for.jpg"
-                    text = "this is small part of artilcle with little sense for"
-                    fullPostLink = "/profile"
-                />
+                </div>
 
-             </div>);
+                <div>
+                    {posts}
+                </div>
+
+            </div>);
     }
 }
