b079392dab85f6c0293bc760f5c9df874019d63f
RanKor <randomkorrektor@gmail.com>
2017-04-01 00:47:47 +0700

    Fix header.

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -9,9 +9,56 @@ import {
 export default class Header extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            user: null
+        }
     }
 
+    async componentDidMount() {
+        const session = localStorage.getItem('session');
+        if (session) {
+            const auth = await fetch(`http://192.168.0.102:3000/api/user?session=${session}`)
+                .then(r => r.json())
+                .then((r) => {
+                    if (r.success) {
+                        return r.data;
+                    }
+                    throw r.error
+                });
+            this.setState({
+                user: auth
+            });
+        }
+    }
+    logout() {
+        localStorage.removeItem('session');
+        this.setState({
+            user: null
+        });
+    }
     render() {
+        let right;
+        if (this.state.user) {
+            right = (<Nav pullRight={true}>
+                <NavItem href="#">
+                    <img className="Navbar_Avatar" src={this.state.user.picture} />
+                </NavItem>
+                <NavDropdown title={this.state.user.name} className="DropHigh UserName" >
+                    <MenuItem >Профиль</MenuItem>
+                    <MenuItem onClick={this.logout.bind(this)}>Выход</MenuItem>
+                </NavDropdown>
+            </Nav>);
+        } else {
+
+            right = (<Nav pullRight={true}>
+                <NavItem href="#/signup">
+                    Зарегестрироваться
+                </NavItem>
+                <NavItem href="#/signin">
+                    Войти
+                </NavItem>
+            </Nav>);
+        }
         return (<Navbar fluid={true} className="NavigationBar">
             <Navbar.Header className="Logo">
                 <img src="/style/img/Le_Bona3.png" />
@@ -22,15 +69,7 @@ export default class Header extends React.Component {
                 <NavItem href="#/contacts" className="Navbar_font_element">Контакты</NavItem>
                 <NavItem href="#/cart" className="Navbar_font_element">Корзина</NavItem>
             </Nav>
-            <Nav pullRight={true}>
-                <NavItem href="#">
-                    <img className="Navbar_Avatar" src="/style/img/noavatar.png" />
-                </NavItem>
-                <NavDropdown title="User name" className="DropHigh UserName" >
-                    <MenuItem >Профиль</MenuItem>
-                    <MenuItem>Выход</MenuItem>
-                </NavDropdown>
-            </Nav>
+            {right}
         </Navbar>);
 
     }
--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,108 +1,110 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 import {
     Link
 } from 'react-router';
 
 
 export class ProductData extends React.Component {
-               
+
     constructor(props) {
         super(props);
     }
 
-    render () {
+    render() {
         return (
             <div>
                 <img src={this.props.imgSrc} className="img-responsive" />
                 <h1> {this.props.title} </h1>
                 <h2> {this.props.price} </h2>
-            </div> 
+            </div>
         );
     }
-   
+
 }
 
 export default class ProductsPage extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                            productData: [
-                                            {
-                                                title: "DatFace",
-                                                price: "25$",
-                                                imgSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg",
-                                                productLink: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg"
-                                            },
+    constructor(props) {
+        super(props);
+        this.state = {
+            productData: [
+                {
+                    title: "DatFace",
+                    price: "25$",
+                    imgSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg",
+                    productLink: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg"
+                },
 
-                                            {
-                                                title: "RapeFace2",
-                                                price: "42$",
-                                                imgSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg",
-                                                productLink: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg"
-                                            },
+                {
+                    title: "RapeFace2",
+                    price: "42$",
+                    imgSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg",
+                    productLink: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg"
+                },
 
-                                            {
-                                                title: "CornFace3",
-                                                price: "16$",
-                                                imgSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg",
-                                                productLink: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg"
-                                            },
+                {
+                    title: "CornFace3",
+                    price: "16$",
+                    imgSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg",
+                    productLink: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg"
+                },
 
-                                            {
-                                                title: "BurnFace4",
-                                                price: "31$",
-                                                imgSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg",
-                                                productLink: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg"
-                                            }
-                                         ]
-                        };
-        this.addToCart = this.addToCart.bind(this);               
+                {
+                    title: "BurnFace4",
+                    price: "31$",
+                    imgSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg",
+                    productLink: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSn9tUr2djnjXjjxWGSXhQYhb_rI-s1uBg-fS4h-zrs48aKfwqOMg"
+                }
+            ]
+        };
+        this.addToCart = this.addToCart.bind(this);
     }
 
- addToCart(number) {   
-    
-    var stateProductData = this.state.productData[number]
-    var title = stateProductData.title //as key
-    if (!localStorage.getItem(title))
-    {
-        var productData = {
-        price: stateProductData.price,
-        cartImg: stateProductData.imgSrc,
-        //count: 1
-        }
+    addToCart(number) {
+
+        var stateProductData = this.state.productData[number]
+        var title = stateProductData.title //as key
+        if (!localStorage.getItem(title)) {
+            var productData = {
+                price: stateProductData.price,
+                cartImg: stateProductData.imgSrc,
+                //count: 1
+            }
             var serialObj = JSON.stringify(productData);
             localStorage.setItem(title, serialObj);
         }
-    //for more than one 
-    /*
-        else {
-        var objForUp = JSON.parse(localStorage.getItem(title));
-        objForUp.count++;
-        var objForUpInString = JSON.stringify(objForUp);
-        localStorage.setItem(title, objForUpInString);
-    }
-    */
-}
-
-        render() {
-             const products = this.state.productData.map((product, i) =>
-                    <div>
-                    <ProductData 
-                        key={i}
-                        title={product.title}
-                        price={product.price}
-                        imgSrc={product.imgSrc}
-                        productLink={product.productLink}
-                     />
-                     <button onClick={this.addToCart.bind(this, i)}>
-                        В КОРЗИНУ
-                    </button>   
-                     </div>)
-
-            return (<div>
-                    {products}
-            </div>);
+        //for more than one 
+        /*
+            else {
+            var objForUp = JSON.parse(localStorage.getItem(title));
+            objForUp.count++;
+            var objForUpInString = JSON.stringify(objForUp);
+            localStorage.setItem(title, objForUpInString);
         }
-    
+        */
+    }
+
+    render() {
+        const products = this.state.productData.map((product, i) =>
+            <div>
+                <ProductData
+                    key={i}
+                    title={product.title}
+                    price={product.price}
+                    imgSrc={product.imgSrc}
+                    productLink={product.productLink}
+                />
+                <button onClick={this.addToCart.bind(this, i)}>
+                    В КОРЗИНУ
+                    </button>
+            </div>)
+
+        return (<div>
+            <Header />
+            {products}
+            <Footer />
+        </div>);
+    }
+
 }
