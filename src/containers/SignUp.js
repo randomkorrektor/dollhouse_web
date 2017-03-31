c658a662b52542b30d062cd8017caf24acd0982b
RanKor <randomkorrektor@gmail.com>
2017-03-31 23:21:16 +0700

    Create signup

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -14,41 +14,49 @@ export default class SignUpPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: " ",
-            password: " ",
-            email: " ",
-            subscribeCheck: "true",
-            message: " "
+            name: "",
+            password1: "",
+            password2: "",
+            email: "",
+            message: ""
         };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    handleInputChange(event) {
+    handleInputChange(name, event) {
+        this.setState({ [name]: event.target.value });
+    }
 
-        const target = event.target;
-
-        if (target.type == 'checkbox') {
-            this.setState({ subscribeCheck: target.checked });
-        }
-        else {
-            if (target.id != emailInput) {
-                if (passwordInput.value == password2Input.value) {
-                    this.setState({ message: "" });
-                }
-                else {
-                    this.setState({ message: "Пароли должны совпадать" });
-                }
+    async handleToggleClick(event) {
+        if (this.state.password1 != this.state.password2) {
+            alert('Пароли не совпадают');
+            return;
+        } else {
+            try {
+                const auth = await fetch('http://192.168.0.102:3000/api/auth', {
+                    headers: new Headers({
+                        "Content-Type": "application/json"
+                    }),
+                    body: JSON.stringify({
+                        email: this.state.email,
+                        password: this.state.password1,
+                        name: this.state.name,
+                    }),
+                    method: 'post'
+                })
+                    .then(r => r.json())
+                    .then((r) => {
+                        if (r.success) {
+                            return r.data;
+                        }
+                        throw r.error
+                    });
+                localStorage.setItem('session', auth.session._id);
+                console.log(auth);
+                window.location.replace('/#/products')
+            } catch (e) {
+                alert(e);
             }
-
-            const value = target.value;
-            const name = target.name;
-            this.setState({ [name]: value });
         }
-    }
-
-    handleToggleClick(event) {
-
     }
 
     render() {
@@ -62,25 +70,30 @@ export default class SignUpPage extends React.Component {
                                 <form role="form">
                                     <h2 className="Font_Caption">Регистрация</h2>
                                     <h3 className="Font_main">Введите имя</h3>
-                                    <input type="email" className="form-control form_border" required autofocus />
+                                    <input type="email" className=" form-control form_border" required autofocus id="nameInput"
+                                        name="name"
+                                        type="text"
+                                        onChange={this.handleInputChange.bind(this, 'name')} />
                                     <h3 className="Font_main">E-mail</h3>
                                     <input type="email" className="form-control form_border" required autofocus id="emailInput"
                                         name="email"
                                         type="text"
-                                        onChange={this.handleInputChange} />
+                                        onChange={this.handleInputChange.bind(this, 'email')} />
                                     <h3 className="Font_main">Пароль</h3>
                                     <input type="password" className="form-control form_border" required
                                         id="passwordInput"
                                         name="password"
                                         type="password"
-                                        onChange={this.handleInputChange} />
+                                        onChange={this.handleInputChange.bind(this, 'password1')} />
                                     <h3 className="Font_main">Повторите пароль</h3>
                                     <input type="password" className="form-control form_border" required
                                         id="password2Input"
                                         name="password2"
                                         type="password"
-                                        onChange={this.handleInputChange} />
-                                    <li id="Sign_up" onClick={this.handleToggleClick}><a href="#" id="Sign_in">Зарегистрироваться</a></li>
+                                        onChange={this.handleInputChange.bind(this, 'password2')} />
+                                    <li id="Sign_up" onClick={this.handleToggleClick.bind(this)}>
+                                        <span id="Sign_in">Зарегистрироваться</span>
+                                    </li>
                                     <label id="passCheckLabel" >
                                         <h4> {this.state.message} </h4>
                                     </label>
